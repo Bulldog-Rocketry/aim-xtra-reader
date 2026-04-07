f1c41b34fcb73ca09396542f0a28258505efeb85
Sam Larson <lars8175@d.umn.edu>
2026-04-07 15:33:46 -0500

    debugging!

--- aimxtra_hidreader.py
+++ aimxtra_hidreader.py
@@ -2,8 +2,9 @@ import time
 
 import hid
 import aim_parser
+# import packets
 
-parser = aim_parser.AimParser(aim_parser.PacketStructureManager())
+parser = aim_parser.AimParser()
 
 dev = hid.device()
 # for some reason the info changes so
@@ -18,9 +19,12 @@ while True:
     dev.write([0x03, 0x12] + [0] * 62)
     d = dev.read(64)
     if d != oldData:
+        print([(hex(x) if 1 < x < 21 else str(x)) for x in d])
         packets = []
-        # packets = parser.parse_transmission(bytes(d))
-        # print(packets)
-        print(d)
+        packets = parser.parse_transmission(bytes(d))
+        print(packets)
+        # for packet in packets:
+        #     print(packet.field_data[2])
+        # print(d)
     oldData = d
 
---
+++ staticReader.py
@@ -0,0 +1,15 @@
+import packets
+import aim_parser
+
+parser = aim_parser.AimParser()
+
+for packet in packets.packets:
+    print(packet)
+    parsed = parser.parse_transmission(bytes(packet))
+    # print()
+    # print(parsed)
+    for item in parsed:
+        print("item: ")
+        print(item)
+    print()
+
